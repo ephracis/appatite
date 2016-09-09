2ef372cd12f0e9bb398d41e372e0fac41a41aa0b
Christoffer Reijer <ephracis@gmail.com>
2016-09-09 21:43:57 +0200

    Make users follow projects (#73)
    Projects are now followable and users are able to be
    followers. When a user activates a project, they follow
    it.
    
    When the owner deactivates a project, the webhook is
    removed.
    
    Editing of projects is restricted to owners and admins and
    only admins are allowed to completely remove projects
    from the database.
    
    This fixes #35

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -1,12 +1,12 @@
 class ProjectsController < ApplicationController
   before_action :authenticate_user!, except: :webhook
-  before_action :set_project, only: [:show, :edit, :update, :destroy]
+  before_action :set_project, only: [:show, :edit, :update]
   skip_before_action :verify_authenticity_token, only: :webhook
 
   # GET /projects
   # GET /projects.json
   def index
-    @projects = current_user.projects.where(active: true).order(:id)
+    @projects = current_user.following_projects.where(active: true).order(:id)
     redirect_to(setup_projects_path) && return unless @projects.any?
   end
 
@@ -63,17 +63,17 @@ class ProjectsController < ApplicationController
   # POST /projects
   # POST /projects.json
   def create
-    @project = Project.where(
-      api_url: project_params[:api_url]
-    ).first_or_initialize
-    @project.user = current_user
-    @project.origin = project_params[:origin]
-    @project.origin_id = project_params[:origin_id]
+    @project = Project.where(api_url: project_params[:api_url])
+                      .first_or_initialize do |p|
+      p.assign_attributes(project_params)
+      p.user = current_user
+    end
     @project.refresh
     @project.active = true
 
     respond_to do |format|
       if @project.save
+        current_user.follow @project
         @project.create_hook(webhook_projects_url) # TODO: send notification on error
         format.html { redirect_to @project, notice: 'Project was successfully created.' }
         format.json { render :show, status: :created, location: @project }
@@ -108,10 +108,18 @@ class ProjectsController < ApplicationController
   # DELETE /projects/1
   # DELETE /projects/1.json
   def destroy
-    @project.delete_hook(webhook_projects_url)
-    @project.destroy
+    @project = get_project_from_collection(Project)
+    notice, redirect_url = unfollow_project
+
+    if current_user.admin? && params[:force]
+      notice, redirect_url = destroy_project
+
+    elsif @project.user == current_user && @project.followers_count.zero?
+      notice, redirect_url = deactivate_project
+    end
+
     respond_to do |format|
-      format.html { redirect_to projects_url, notice: 'Project was successfully destroyed.' }
+      format.html { redirect_to redirect_url, notice: notice }
       format.json { head :no_content }
     end
   end
@@ -120,16 +128,22 @@ class ProjectsController < ApplicationController
 
   # Use callbacks to share common setup or constraints between actions.
   def set_project
-    @project = if params[:api_url]
-                 Project.find_by api_url: params[:api_url]
-               elsif params[:origin] && params[:origin_id]
-                 Project.find_by(
-                   origin: params[:origin],
-                   origin_id: params[:origin_id]
-                 )
-               else
-                 Project.find(params[:id])
-               end
+    @project = get_project_from_collection(
+      current_user.admin? ? Project : current_user.projects
+    )
+  end
+
+  def get_project_from_collection(projects)
+    if params[:api_url]
+      projects.find_by api_url: params[:api_url]
+    elsif params[:origin] && params[:origin_id]
+      projects.find_by(
+        origin: params[:origin],
+        origin_id: params[:origin_id]
+      )
+    else
+      projects.find(params[:id])
+    end
   end
 
   # Never trust parameters from the scary internet, only allow the white list through.
@@ -137,5 +151,22 @@ class ProjectsController < ApplicationController
     params.require(:project).permit(
       :active, :origin, :origin_id, :coverage, :build_state, :name, :api_url
     )
+  end
+
+  def unfollow_project
+    current_user.stop_following @project
+    ["You stopped following #{@project.name}", project_url(@project)]
+  end
+
+  def deactivate_project
+    @project.delete_hook(webhook_projects_url)
+    @project.update_attribute(:active, false)
+    ['Project was successfully deactivated', projects_url]
+  end
+
+  def destroy_project
+    @project.delete_hook(webhook_projects_url)
+    @project.destroy
+    ['Project was successfully destroyed', projects_url]
   end
 end
