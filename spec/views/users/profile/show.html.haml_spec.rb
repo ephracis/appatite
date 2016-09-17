4cdb7e731578d5c7b501d110e29c8b36b69b21c0
Patrik Svartholm <patrik.svartholm@basalt.se>
2016-09-17 20:49:39 +0200

    Handle missing location and website
    Link should only be displayed when it is not empty.
    
    If user has not set location, it should display default location.
    
    Closes #91

--- spec/views/users/profile/show.html.haml_spec.rb
+++ spec/views/users/profile/show.html.haml_spec.rb
@@ -9,18 +9,32 @@ describe 'users/profile/show' do
                                 website: 'http://foo.bar',
                                 nickname: 'awesome-user',
                                 location: 'testland')
-    render layout: 'layouts/profile', template: '/users/profile/show'
   end
 
   it 'should display user name' do
+    render layout: 'layouts/profile', template: '/users/profile/show'
     expect(rendered).to have_content 'Awesome User'
   end
 
+  it 'should state default location if missing' do
+    assign :user, create(:user, location: '')
+    render layout: 'layouts/profile', template: '/users/profile/show'
+    expect(rendered).to have_content 'All over the place'
+  end
+
   it 'should display avatar' do
+    render layout: 'layouts/profile', template: '/users/profile/show'
     expect(rendered).to have_selector "img[src='http://img.com/jpg']"
   end
 
   it 'should display overview' do
+    render layout: 'layouts/profile', template: '/users/profile/show'
     expect(rendered).to have_content 'Nice overview'
+  end
+
+  it 'should not display website when missing' do
+    assign :user, create(:user, website: '')
+    render layout: 'layouts/profile', template: '/users/profile/show'
+    expect(rendered).to_not have_selector '.panel span.fa-link'
   end
 end
