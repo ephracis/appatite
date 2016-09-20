fe29afd2ee4465708b6a4286c6f962d5c0acba93
Christoffer Reijer <ephracis@gmail.com>
2016-09-20 20:51:09 +0200

    Refactor main layout (#97)
    - Split layout into multiple files:
      - HTML head
      - Navigation bar
      - Footer
    
    Switch bootstrap for bootstrap-sass so we can modify it using the
    built in variables.
    
    Closes #96

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -13,7 +13,7 @@
 //= require jquery
 //= require jquery_ujs
 //= require turbolinks
-//= require bootstrap
+//= require bootstrap-sass
 //= require angular2
 //= require angular-fullscreen
 //= require_tree .
